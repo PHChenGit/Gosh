e09cec76a21a6d31d8da010fd1509e1f6a47852a
PHChenGit <chenpohsun1@gmail.com>
2021-02-20 22:27:02 +0800

    Feat: Run basic linux commands

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		input, _ := stdin.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		err := execInput(input)
+
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func execInput(input string) error {
+	cmd := exec.Command(input)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+
+	return err
+}
