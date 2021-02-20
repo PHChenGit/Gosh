7307a0293321e6a76ebd4096ec3927355386ddaa
PHChenGit <chenpohsun1@gmail.com>
2021-02-20 23:22:29 +0800

    Feat: Extend commands

--- main.go
+++ main.go
@@ -28,10 +28,26 @@ func main() {
 }
 
 func execInput(input string) error {
+	input = os.ExpandEnv(input)
 	args := strings.Split(input, " ")
 
 	if args[0] == "cd" {
 		err := os.Chdir(args[1])
+
+		return err
+	}
+
+	if args[0] == "export" {
+		kv := strings.Split(args[1], "=")
+
+		err := os.Setenv(kv[0], kv[1])
+
+		return err
+	}
+
+	if args[1] == "unset" {
+		key := args[1]
+		err := os.Unsetenv(key)
 
 		return err
 	}
